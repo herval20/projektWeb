f4871c7692c63d798b898e655f804805b9f1a3ef
herval20 <herval20@htl-shkoder.com>
2025-04-19 17:36:52 +0200

    created like button

--- src/routes/articles/[id]/+page.svelte
+++ src/routes/articles/[id]/+page.svelte
@@ -8,8 +8,14 @@
 <a href="/">← Back</a>
 
 	{#each data.articles as article (article.id)}
-		<div class="box" transition:slide>
+		<div transition:slide>
             <img src={article.image} alt="uploadedImage" />
 			<p>{article.author} - {article.description} - {article.votes}</p>
+            <form action="?/like" method="POST" use:enhance>
+                <input type="hidden" name="id" value={article.id} />
+                <button type="submit" aria-label="Like">
+                <img src="https://mrg7lubbiiexsyqp.public.blob.vercel-storage.com/projektPhotos/like-kYOU8svw09AFPoplrkMjE2NsWWFQY5.png" alt="like">
+                </button>
+            </form>
 		</div>
 	{/each}
