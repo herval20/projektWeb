5a58c359e02956e70582af9588fec3ae8a9c892e
herval20 <herval20@htl-shkoder.com>
2025-04-19 16:40:17 +0200

    created get and post for api

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,26 +1,20 @@
 import mysql from 'mysql2/promise';
-import {
 
-	DB_PORT,
-	DB_PASSWORD,
-	DB_NAME,
-	DB_HOST,
-    DB_USER} from '$env/static/private';
+import { 
+	DB_HOST, 
+	DB_USER, 
+	DB_PORT, 
+	DB_PASSWORD, 
+	DB_NAME } 
+	
+	from '$env/static/private';
 
-
-
-
-let connection = null;
-
-export function createConnection() {
-	if (!connection) {
-		connection = mysql.createConnection({
-			host: DB_HOST,
-			user: DB_USER,
-			port: DB_PORT,
-			password: DB_PASSWORD,
-			database: DB_NAME
-		});
-	}
-	return connection;
+export async function createConnection() {
+ return mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  port: DB_PORT,
+  password: DB_PASSWORD,
+  database: DB_NAME
+ });
 }
